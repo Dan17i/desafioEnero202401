5b831ab55d50bc30cb6557b734fecf4fad3aa141
Jurden <daniele.juradoc@uqvirtual.edu.co>
2024-03-26 14:29:57 -0500

    eliminar atributos clase

--- src/main/java/com/programandoenjava/parte3/clases/Biblioteca.java
+++ src/main/java/com/programandoenjava/parte3/clases/Biblioteca.java
@@ -4,13 +4,10 @@ import java.util.HashSet;
 
 public class Biblioteca {
     private Collection<Libro> listaLibros;
-    private Libro libro;
-    private Persona persona;
+
 
     public Biblioteca (){
         this.listaLibros= new HashSet<>();
-        this.libro = libro;
-        this.persona = persona;
     }
 
     public void agregarLibro (Libro libro){
